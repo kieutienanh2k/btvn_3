3dbcbb87555ca69e9d410285cfc3fb7811f760f9
kieutienanh2k <56117896+kieutienanh2k@users.noreply.github.com>
2022-09-06 01:18:38 +0700

    create project

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Btvn_3.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            TempData["Name"] = "string_name";
+            TempData["Age"] = "string_age";
+            TempData["Address"] = "string_address";
+
+            return View("ShowData");
+        }
+
+        public ActionResult ShowData()
+        {
+            return View();
+        }
+    }
+}
